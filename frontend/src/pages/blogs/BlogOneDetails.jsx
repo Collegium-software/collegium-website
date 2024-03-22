eb921bae4587c9d2dc8424021bae705211e8906f
sadiqbehzad <147103374+sadiqbehzad@users.noreply.github.com>
2024-03-22 12:56:44 -0600

    server config and blogs fix

--- frontend/src/pages/blogs/BlogOneDetails.jsx
+++ frontend/src/pages/blogs/BlogOneDetails.jsx
@@ -45,81 +45,82 @@ const BlogOneDetails = () => {
   );
 
   return (
-    <div className="blogpagedetails">
+    <>
       <InterimNavbarBlog background="white" color="black" />
-      {loading ? (
-        <div className="loading-waitTime">
-          <img src={waitForFetch} />
-        </div>
-      ) : (
-        <>
-          <div className="blog-hero-pic">
-            <img
-              src={`data:image/jpeg;base64,${allBlogs[0].image}`}
-              alt={allBlogs[0].title}
-            />
+      <div className="blogpagedetails">
+        {loading ? (
+          <div className="blogdetails-loading-waitTime">
+            <img src={waitForFetch} />
           </div>
-          <div className="blogpage-full-article">
-            <div className="blogpageFullArticle-title">
-              <h2>{allBlogs[0].title}</h2>
-              <p>
-                <strong>Written by: {allBlogs[0].author}</strong>
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{allBlogs[0].date}
-              </p>
+        ) : (
+          <>
+            <div className="blog-hero-pic">
+              <img
+                src={`data:image/jpeg;base64,${allBlogs[0].image}`}
+                alt={allBlogs[0].title}
+              />
             </div>
-            <div className="blogpageFullArticle-description">
-              <p>{allBlogs[0].description}</p>
+            <div className="blogpage-full-article">
+              <div className="blogpageFullArticle-title">
+                <h2>{allBlogs[0].title}</h2>
+                <p>
+                  <strong>Written by: {allBlogs[0].author}</strong>
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{allBlogs[0].date}
+                </p>
+              </div>
+              <div className="blogpageFullArticle-description">
+                <p>{allBlogs[0].description}</p>
+              </div>
             </div>
-          </div>
-          <div className="recent-posts">
-            <h1>Other Blogs:</h1>
-            <div className="blogs">
-              {HToV &&
-                filteredBlogs.slice(0, 3).map((blog) => (
-                  <div key={blog.id}>
-                    <BlogHorizontal
-                      id={blog.id}
-                      image={blog.image}
-                      title={blog.title}
-                      author={blog.author}
-                      description={blog.description}
-                      date={blog.date}
-                      bttnLabel={blog.button.label}
-                      bttnColor={blog.button.color}
-                      bttnTo={blog.button.to}
-                    />
-                  </div>
-                ))}
-              {vToH &&
-                filteredBlogs.slice(0, 3).map((blog) => (
-                  <div key={blog.id}>
-                    <BlogVertical
-                      id={blog.id}
-                      image={blog.image}
-                      title={blog.title}
-                      author={blog.author}
-                      description={blog.description}
-                      date={blog.date}
-                      bttnLabel={blog.button.label}
-                      bttnColor={blog.button.color}
-                      bttnTo={blog.button.to}
-                    />
-                  </div>
-                ))}
+            <div className="recent-posts">
+              <h1>Other Blogs:</h1>
+              <div className="blogs">
+                {HToV &&
+                  filteredBlogs.slice(0, 3).map((blog) => (
+                    <div key={blog.id}>
+                      <BlogHorizontal
+                        id={blog.id}
+                        image={blog.image}
+                        title={blog.title}
+                        author={blog.author}
+                        description={blog.description}
+                        date={blog.date}
+                        bttnLabel={blog.button.label}
+                        bttnColor={blog.button.color}
+                        bttnTo={blog.button.to}
+                      />
+                    </div>
+                  ))}
+                {vToH &&
+                  filteredBlogs.slice(0, 3).map((blog) => (
+                    <div key={blog.id}>
+                      <BlogVertical
+                        id={blog.id}
+                        image={blog.image}
+                        title={blog.title}
+                        author={blog.author}
+                        description={blog.description}
+                        date={blog.date}
+                        bttnLabel={blog.button.label}
+                        bttnColor={blog.button.color}
+                        bttnTo={blog.button.to}
+                      />
+                    </div>
+                  ))}
+              </div>
             </div>
-          </div>
-          <div className="see-more-button">
-            <Button
-              label="See more blogs"
-              color="black"
-              to="/blogs/all-blogs"
-            />
-          </div>
-        </>
-      )}
-
+            <div className="see-more-button">
+              <Button
+                label="See more blogs"
+                color="black"
+                to="/blogs/all-blogs"
+              />
+            </div>
+          </>
+        )}
+      </div>
       <SFooter />
-    </div>
+    </>
   );
 };
 
